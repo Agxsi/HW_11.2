b8d53588faaf46844c04b945addaa0872fe06702
khach <khachikyan.agasi@mail.ru>
2023-04-05 22:00:43 +0300

    changes in function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_upper(word):
+    """Слово полностью с заглавной буквы"""
     return word.upper()
+
+
+def get_title(word):
+    """Возвращает слово с заглавной буквы"""
+    return word.title()
